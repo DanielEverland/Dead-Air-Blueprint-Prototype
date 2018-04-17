ecff1a0c67b153b58bfb869fe8acefbb38099bbf
Daniel Everland <danieleverland@gmail.com>
2018-04-17 17:32:12 +0200

    Added battery

---
+++ Assets/Scripts/Items/Properties/Battery.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : PropertyBase, IPropertyOutput {
+
+    public Battery(ItemBase owner) : base(owner) { }
+
+    public PropertyEventTypes OutputTypes { get { return PropertyEventTypes.OnElectricalInput; } }
+
+    public override void Update()
+    {
+        Owner.RaiseEvent(PropertyEventTypes.OnElectricalInput, null);
+    }
+}
