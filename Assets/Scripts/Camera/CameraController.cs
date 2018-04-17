0156cf7a5ba88ed8c19a5c79ebcde41ec3b1e0b3
Daniel Everland <danieleverland@gmail.com>
2018-04-17 06:55:30 +0200

    Added camera controller

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+    private float _movementSpeed = 5;
+    [SerializeField]
+    private GameObject _player;
+    [SerializeField]
+    private Vector3 _offset = new Vector3(0, 0, -10);
+
+    private void Awake()
+    {
+        if (_player == null)
+        {
+            throw new System.NullReferenceException("Missing Player field on CameraController");
+
+            enabled = false;
+        }            
+    }
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, _player.transform.position, _movementSpeed * Time.deltaTime) + _offset;
+    }
+}
