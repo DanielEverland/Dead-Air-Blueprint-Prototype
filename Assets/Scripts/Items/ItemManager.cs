e106cf9985245800327771adc07aa72502584fdf
Daniel Everland <danieleverland@gmail.com>
2018-04-17 17:07:39 +0200

    Added ability to spawn items

---
+++ Assets/Scripts/Items/ItemManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ItemManager {
+    
+    public static void Spawn(ItemBase item)
+    {
+        GameObject obj = item.CreateObject();
+        ItemObject container = obj.AddComponent<ItemObject>();
+
+        container.Item = item;
+
+        item.RaiseEvent(PropertyEventTypes.OnItemCreated, null);
+    }
+    public static void Test()
+    {
+        ItemBase item = new ItemBase(ReflectionManager.PropertyTypes.ToArray());
+
+        Spawn(item);
+    }
+}
---
+++ Assets/Scripts/Items/ItemObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour {
+
+    public ItemBase Item { get; set; }
+
+    private void Update()
+    {
+        Item.Update();
+    }
+}
