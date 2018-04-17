b52cafdd27ec65526dec1cb31b82bc84a1aab934
Daniel Everland <danieleverland@gmail.com>
2018-04-17 06:39:39 +0200

    Init for item system

---
+++ Assets/Scripts/Items/IItemProperty.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IItemProperty {
+
+    /// <summary>
+    /// Callback allowing a property to add/get components
+    /// on the object it's been added to
+    /// </summary>
+    void CreateInstance(GameObject obj);
+}
---
+++ Assets/Scripts/Items/ItemBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase {
+
+    /// <summary>
+    /// Combines items
+    /// </summary>
+    public ItemBase(params ItemBase[] items)
+    {
+        Merge(items);
+    }
+
+    public IEnumerable<IItemProperty> Properties { get { return _properties; } }
+
+    private List<IItemProperty> _properties;
+
+    public GameObject CreateObject()
+    {
+        GameObject obj = new GameObject();
+
+        foreach (IItemProperty property in _properties)
+        {
+            property.CreateInstance(obj);
+        }
+
+        return obj;
+    }
+
+    /// <summary>
+    /// Copies the properties of <paramref name="items"/> into the current instance
+    /// </summary>
+    /// <param name="items"></param>
+    public void Merge(IEnumerable<ItemBase> items)
+    {
+        foreach (ItemBase item in items)
+        {
+            _properties.AddRange(item.Properties);
+        }
+    }
+}
