573d97edc2ddeae9b6acf30a8e09fefb89539c17
Daniel Everland <danieleverland@gmail.com>
2018-04-17 16:24:47 +0200

    Added proper events to Timer

--- Assets/Scripts/Items/Properties/Timer.cs
+++ Assets/Scripts/Items/Properties/Timer.cs
@@ -3,10 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Timer : PropertyBase, IPropertyInput {
+public class Timer : PropertyBase, IPropertyInput, IPropertyOutput {
 
     public Timer(ItemBase owner) : base(owner) { }
 
-    public PropertyEventTypes InputTypes { get { return PropertyEventTypes.OnItemCreated; } }
-    
+    public PropertyEventTypes InputTypes { get { return PropertyEventTypes.OnPlacedInWorld; } }
+    public PropertyEventTypes OutputTypes { get { return PropertyEventTypes.OnTrigger; } }
+
+    private void OnPlacedInWorld()
+    {
+
+    }
 }
