d341e757b6b13a58e289899971d29902278f8b5c
Daniel Everland <danieleverland@gmail.com>
2018-04-19 22:43:32 +0200

    Created add button

---
+++ Assets/Scripts/UI/ItemCreatePanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCreatePanel : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _panel;
+
+    private void Update()
+    {
+        PollToggle();
+    }
+    private void PollToggle()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            _panel.SetActive(!_panel.activeInHierarchy);
+        }
+    }
+    public void CreateItem()
+    {
+
+    }
+}
