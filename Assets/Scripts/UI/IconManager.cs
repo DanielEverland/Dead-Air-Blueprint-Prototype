ee5f1267e0ca7dd82c75a90a4880936170537942
Daniel Everland <danieleverland@gmail.com>
2018-04-19 17:38:23 +0200

    Icon selection init

---
+++ Assets/Scripts/UI/IconManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class IconManager {
+
+    public static IEnumerable<Texture2D> Icons
+    {
+        get
+        {
+            if (_icons == null)
+                LoadIcons();
+
+            return _icons;
+        }
+    }
+
+    private static List<Texture2D> _icons;
+
+    private static void LoadIcons()
+    {
+        _icons = new List<Texture2D>(Resources.LoadAll<Texture2D>("Icons"));
+    }
+}
---
+++ Assets/Scripts/UI/IconPanel.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class IconPanel : MonoBehaviour, IPointerClickHandler {
+
+    [SerializeField]
+    private Text _text;
+    [SerializeField]
+    private RawImage _image;
+
+    private Texture2D _selectedIcon;
+
+    private void Start()
+    {
+        Reload();
+    }
+    private void Reload()
+    {
+        bool selected = _selectedIcon != null;
+
+        _text.enabled = !selected;
+        _image.enabled = selected;
+
+        _image.texture = _selectedIcon;
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Test");
+    }
+}
