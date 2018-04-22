e8acc8e5de5514ea63fba016661578558f67b0ac
Daniel Everland <danieleverland@gmail.com>
2018-04-22 23:32:57 +0200

    Removed namespace declaration in LightProperty

--- Assets/Scripts/Items/Properties/LightProperty.cs
+++ Assets/Scripts/Items/Properties/LightProperty.cs
@@ -3,33 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace Assets.Scripts.Items.Properties
+public class LightProperty : PropertyBase, IPropertyInput
 {
-    public class LightProperty : PropertyBase, IPropertyInput
+    public PropertyEventTypes InputTypes { get { return PropertyEventTypes.OnElectricalInputChanged; } }
+
+    public override string Name { get { return "Light"; } }
+
+    private Light _light;
+
+    private void OnElectricalInputChanged()
     {
-        public PropertyEventTypes InputTypes { get { return PropertyEventTypes.OnElectricalInputChanged; } }
+        _light.enabled = Owner.IsElectricallyCharged;
+    }
+    public override void CreateInstance(GameObject obj)
+    {
+        IEnumerable<Component> components = PropertyHelper.CopyComponents("Light", obj);
 
-        public override string Name { get { return "Light"; } }
-
-        private Light _light;
-
-        private void OnElectricalInputChanged()
+        _light = components.First<Light>();
+        _light.enabled = Owner.IsElectricallyCharged;
+    }
+    public override string[] GetInformation()
+    {
+        return new string[1]
         {
-            _light.enabled = Owner.IsElectricallyCharged;
-        }
-        public override void CreateInstance(GameObject obj)
-        {
-            IEnumerable<Component> components = PropertyHelper.CopyComponents("Light", obj);
-
-            _light = components.First<Light>();
-            _light.enabled = Owner.IsElectricallyCharged;
-        }
-        public override string[] GetInformation()
-        {
-            return new string[1]
-            {
                 "Enabled: " + _light.enabled,
-            };
-        }
+        };
     }
 }
