05db648c01adc265340a5e16e1583aca0a2f498d
Daniel Everland <danieleverland@gmail.com>
2018-04-23 01:18:26 +0200

    Added alcohol

---
+++ Assets/Scripts/Items/Properties/Alcohol.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alcohol : LiquidPropertyBase
+{
+    public override string Name { get { return "Alcohol"; } }
+    public override Color32 Color { get { return UnityEngine.Color.white; } }
+}
