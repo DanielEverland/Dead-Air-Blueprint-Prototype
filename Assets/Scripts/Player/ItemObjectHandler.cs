d45e6e65b1ac17e80b9cc11d7b021f4bfd172304
Daniel Everland <danieleverland@gmail.com>
2018-04-22 16:17:40 +0200

    Moved the plane items exists on back

--- Assets/Scripts/Player/ItemObjectHandler.cs
+++ Assets/Scripts/Player/ItemObjectHandler.cs
@@ -13,6 +13,8 @@ public class ItemObjectHandler : MonoBehaviour {
     [SerializeField]
     private float _radius = 1;
 
+    private const float DEPTH = -1;
+
     private static ItemObjectHandler _instance;
 
     private ItemObject _object;
@@ -86,8 +88,14 @@ public class ItemObjectHandler : MonoBehaviour {
         Vector2 vector = GetLocalVector(radians);
 
         Vector2 objPos = vector * _radius;
+        
+        AssignPosition(transform.position + (Vector3)objPos);
+    }
+    private void AssignPosition(Vector3 position)
+    {
+        position.z = DEPTH;
 
-        _object.transform.position = transform.position + (Vector3)objPos;
+        _object.transform.position = position;
     }
     private Vector2 GetLocalVector(float radians)
     {
