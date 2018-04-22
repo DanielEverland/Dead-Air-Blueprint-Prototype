f4cb5f12914d3d2f4dcba98adc86718a67a5325c
Daniel Everland <danieleverland@gmail.com>
2018-04-22 17:33:59 +0200

    Added water bottle

---
+++ Assets/Scripts/Items/Properties/WaterBottle.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBottle : PropertyBase, ILiquidContainerProperty
+{
+    public override string Name { get { return "Water Bottle"; } }
+    
+}
