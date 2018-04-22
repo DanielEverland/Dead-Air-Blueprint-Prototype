aa448fc07f826edf087c937d17a5c4884cfd46f7
Daniel Everland <danieleverland@gmail.com>
2018-04-22 17:22:22 +0200

    Added glass bottle

---
+++ Assets/Scripts/Items/Properties/GlassBottle.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassBottle : PropertyBase, ILiquidContainerProperty, IPropertyInput
+{
+    public override string Name { get { return "Glass Bottle"; } }
+
+    public PropertyEventTypes InputTypes { get { return PropertyEventTypes.OnThrowEnds; } }
+
+    private void OnThrowEnds()
+    {
+        Break();
+    }
+    private void Break()
+    {
+        Owner.Remove(this);
+    }
+}
