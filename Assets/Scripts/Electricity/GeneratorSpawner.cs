47445d5c0c0c7d9d9246b4803dc40b4617951753
Daniel Everland <danieleverland@gmail.com>
2018-04-25 19:35:29 +0200

    Added ability to place generators

---
+++ Assets/Scripts/Electricity/GeneratorSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private Generator _prefab;
+
+    private const float Z_DEPTH = 0;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Generator instance = Instantiate(_prefab);
+
+            Vector3 mousePosInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosInWorld.z = Z_DEPTH;
+
+            instance.transform.position = mousePosInWorld;
+        }
+    }
+}
