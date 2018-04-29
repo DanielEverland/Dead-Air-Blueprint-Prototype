73e9f2c17d83a03aeae563d820e4ef20bd643ef8
Daniel Everland <danieleverland@gmail.com>
2018-04-29 23:50:27 +0200

    Added pressure switch

---
+++ Assets/Scripts/Items/Properties/PressureSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressureSwitch : PropertyBase, IPropertyInput, IPropertyOutput
+{
+    public override string Name { get { return "Pressure Switch"; } }
+
+    public PropertyEventTypes InputTypes { get { return PropertyEventTypes.OnCollisionEnter; } }
+    public PropertyEventTypes OutputTypes { get { return PropertyEventTypes.OnTrigger; } }
+
+    private bool _hasTriggered;
+
+    private void OnCollisionEnter(Transform hit)
+    {
+        if (!_hasTriggered)
+        {
+            _hasTriggered = true;
+            Owner.RaiseEvent(PropertyEventTypes.OnTrigger, null);
+        }        
+    }
+    public override string[] GetInformation()
+    {
+        return new string[1] { "HasTriggered: " + _hasTriggered };
+    }
+}
