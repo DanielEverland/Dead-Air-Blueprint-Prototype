c7e03e36bac30014dd49b5c49b5e0213fe2e478f
Daniel Everland <danieleverland@gmail.com>
2018-04-29 23:57:43 +0200

    Added petrol

---
+++ Assets/Scripts/Items/Properties/Petrol.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Petrol : LiquidPropertyBase
+{
+    public override string Name { get { return "Petrol"; } }
+    public override Color32 Color { get { return UnityEngine.Color.yellow; } }
+    public override bool IsFlammable { get { return true; } }
+}
